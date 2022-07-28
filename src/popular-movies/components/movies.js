54540e162aaf0d912ee878a4ea93864f2adb1b32
Fr1xel <ivan.rados12354@gmail.com>
2022-07-28 18:34:54 +0200

    improvement

--- src/popular-movies/components/movies.js
+++ src/popular-movies/components/movies.js
@@ -1,17 +1,30 @@
-import "./movies.css"
+import "./movies.css";
 import Rating from "../../rating-color";
 
 const Movies = (props) => {
-  const {movies, setModuleIsOpen, setModalMovie} = props
-  return movies.map(movie => {
-    return (
-      <div className="col-lg-3 col-md-6 col-12 cursor-pointer hover-bigger max-width-100 click-info-popular" key = {movie.id} onClick={() => {setModalMovie(movie); setModuleIsOpen(true)}}>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="img-fluid border-radius"></img>
-        <h5 className="p-3 silver-border">{movie.title}</h5>
-        <Rating movie = {movie} />
-      </div>
-    );
-  });
+  const { movies, setModuleIsOpen, setModalMovie } = props;
+
+  return movies.map((mainMovies) =>
+    mainMovies.map((movie) => {
+      return (
+        <div
+          className="col-lg-3 col-md-6 col-12 cursor-pointer hover-bigger max-width-100 click-info-popular"
+          key={movie.id}
+          onClick={() => {
+            setModalMovie(movie);
+            setModuleIsOpen(true);
+          }}
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            className="img-fluid border-radius"
+          ></img>
+          <h5 className="p-3 silver-border">{movie.title}</h5>
+          <Rating movie={movie} />
+        </div>
+      );
+    })
+  );
 };
 
 export default Movies;
--- src/popular-movies/index.js
+++ src/popular-movies/index.js
@@ -10,80 +10,80 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const PopularOrTopRatedMovies = (props) => {
   const { code, setModuleIsOpen, setModalMovie } = props;
   const [movies, setMovie] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const data = await axios.get(
-        `https://api.themoviedb.org/3/movie/${code}?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=${page}`
-      );
-      setMovie(data.data.results);
-      setLoading(false);
-    };
-    fetchPosts();
-  });
+  const [movieBundle, setMovieBundle] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const pushingMovies = (movies, movieBundle) => {
+    movieBundle.length
+      ? movieBundle.forEach((mainMovies) => {
+          JSON.stringify(movies) === JSON.stringify(mainMovies)
+            ? console.log(false)
+            : movieBundle.push(movies);
+          setPage(page + 1);
+        })
+      : movieBundle.push(movies);
+  };
 
   const loadMorePosts = async () => {
-    console.log("im running");
     const data = await axios.get(
       `https://api.themoviedb.org/3/movie/${code}?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=${page}`
     );
-    console.log(data.data.results);
-    //setMovie(data.data.results);
-    //setLoading(false);
+    pushingMovies(data.data.results, movieBundle);
+    console.log(movieBundle);
+    setMovie(data.data.results);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    loadMorePosts();
+  }, []);
   return loading ? (
     <h1>loading...</h1>
   ) : (
-    <InfiniteScroll
+    <div
       className="text-align-start text-light mt-5 d-flex justify-content-center max-width-50rem"
       id="popular"
-      dataLength={movies.length} //This is important field to render the next data
-      next={loadMorePosts}
-      hasMore={true}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
-      // below props only if you need pull down functionality
-      refreshFunction={loadMorePosts}
-      pullDownToRefresh
-      pullDownToRefreshThreshold={50}
     >
       <div className="container">
         <div className="border-radius">
           <h1 className="redder-text">
             Popular Movies <i className="bi bi-bar-chart"></i>
           </h1>
-          <div
+          <InfiniteScroll
+            useWindow={false}
+            scrollThreshold="200px"
             className="row g-5 text-center pt-3 max-width-100 m-auto"
-            id="popularDiv"
+            style={{
+              height: "auto",
+              overflow: "hidden",
+              willChange: "unset",
+              transition: "transform 0.2s cubic-bezier(0, 0, 0.31, 1) 0s",
+              transform: "none",
+            }}
+            id="popular"
+            dataLength={movies}
+            next={loadMorePosts}
+            hasMore={true}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+            // below props only if you need pull down functionality
+            refreshFunction={loadMorePosts}
+            pullDownToRefresh
+            pullDownToRefreshThreshold={50}
           >
             <Movies
-              movies={movies}
+              movies={movieBundle}
               setModuleIsOpen={setModuleIsOpen}
               setModalMovie={setModalMovie}
             />
-          </div>
-          <div className="d-flex justify-content-between">
-            <button
-              className="page-button h3"
-              onClick={() => (page !== 1 ? setPage(page - 1) : setPage(page))}
-            >
-              <i className="bi bi-arrow-left"></i>
-            </button>
-            <p className="h3 color-red">Page: {page}</p>
-            <button
-              className="page-button h3"
-              onClick={() => setPage(page + 1)}
-            >
-              <i className="bi bi-arrow-right"></i>
-            </button>
-          </div>
+          </InfiniteScroll>
         </div>
       </div>
-    </InfiniteScroll>
+    </div>
   );
 };
 
